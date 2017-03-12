66301cb3c852a49c910b27da08f01229f91a35f9
Zixuan Li <armstrong0220@gmail.com>
2017-03-13 03:45:06 +0800

    Finished LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,15 @@ public class LeapYear {
             }
         }
     }
+
+    public static boolean isLeapYear(int year){
+        if (year % 4 != 0){
+            return false;
+        }else if (year % 100 == 0 & year % 400 != 0) {
+            return false;
+        }else {
+            return true;   
+        }
+    }
 }
 
