e44f5510fb93f8cc34d39943178a3938c1b1767d
Zixuan Li <armstrong0220@gmail.com>
2017-03-29 20:20:29 +0800

    Update 2 Proj0 Finished NBody

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,35 @@
-public class NBody {
+public class NBody
+{
+	public static double readRadius(String dir)
+	{
+		In in = new In(dir);
+		in.readInt();
+		double radius = in.readDouble();
+		return radius;
+	}
+
+	public static Planet[] readPlanets(String dir)
+	{
+		In in = new In(dir);
+		in.readInt();
+		in.readDouble();
+		
+		Planet[] planets = new Planet[5];
+
+		for(int i = 0; i < 5; i ++)
+		{
+			double xxPos = in.readDouble();
+			double yyPos = in.readDouble();
+			double xxVel = in.readDouble();
+			double yyVel = in.readDouble();
+			double mass = in.readDouble();
+			String imgFileName = in.readString();
+
+			Planet p = new Planet(xxPos,yyPos,xxVel,yyVel,mass,imgFileName);
+			planets[i] = p;
+		}
+
+		return planets;
+	}
 
 }
